537fc6815e9a6171f1488bda99b95fe5722b4dd4
Michał Haładus <scovilsyn@gmail.com>
2025-11-25 00:54:13 +0100

    Fix zadanie 4

---
+++ php_04_obiektowosc/app/calc.php
@@ -0,0 +1,10 @@
+<?php
+require_once dirname(__FILE__).'/../config.php';
+
+// Ochrona aplikacji
+include $conf->root_path.'/app/security/check.php';
+
+require_once $conf->root_path.'/app/CalcCtrl.class.php';
+
+$ctrl = new CalcCtrl();
+$ctrl->process();
---
+++ php_04_obiektowosc/app/security/logout.php
@@ -0,0 +1,9 @@
+<?php
+require_once dirname(__FILE__).'/../../config.php';
+
+// 1. zakończenie sesji
+session_start();
+session_destroy();
+
+// 2. przekieruj lub "forward" na stronę główną
+header("Location: ".$conf->app_url);
